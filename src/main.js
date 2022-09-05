021e198230572b9012d206e9ddc7a463f2d64195
Samvel Sargsyan <samvelsargsyan0995@gmail.com>
2022-09-05 23:41:32 +0400

    Bug fixing

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import { createApp } from 'vue'
-import '@dafcoe/vue-collapsible-panel/dist/vue-collapsible-panel.css'
 import App from './App.vue'
 import router from './router'
 import store from './store'
